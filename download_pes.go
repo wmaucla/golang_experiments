c9e1086587060f856248bb3de8f2dfb49a8082e0
William <williammaucla@gmail.com>
2021-08-03 21:03:08 -0400

    feat: adding in an enhanced example with more ids

--- download_pes.go
+++ download_pes.go
@@ -4,20 +4,30 @@ import (
 	"fmt"
 	"github.com/anaskhan96/soup"
 	"os"
+	"strconv"
 )
 
 func main() {
-	resp, err := soup.Get("https://pesdb.net/pes2021/?id=134815") 
-	if err != nil {
-		os.Exit(1)
-	}
-	doc := soup.HTMLParse(resp)
-	links := doc.FindAllStrict("tr", "data-percent", "100")
-	for _, link := range links {
-		var one_user []string // create a slice
-		for _, x := range(link.Children()){
-			one_user = append(one_user, x.FullText())
+
+	user_map := map[int][][]string {}
+	player_ids_array := []int{134815, 133171}
+	for _, player_id := range player_ids_array {
+		resp, err := soup.Get("https://pesdb.net/pes2021/?id=" + strconv.Itoa(player_id))
+		
+		if err != nil {
+			os.Exit(1)
 		}
-		fmt.Println(one_user)
+		doc := soup.HTMLParse(resp)
+		links := doc.FindAllStrict("tr", "data-percent", "100")
+		var one_user [][]string // create a slice to hold data for one player
+		for _, link := range links {
+			var one_row []string 
+			for _, x := range(link.Children()){
+				one_row = append(one_row, x.FullText()) // add a row of data
+			}
+			one_user = append(one_user, one_row) // add the row of data into that player
+		}
+		user_map[player_id] = one_user // assign to a map with key player id
 	}
+	fmt.Println(user_map)
 }
